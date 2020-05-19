d59fc874f23a7cd4df3ce7092ca9c9f2a1309019
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-05-19 23:37:32 +0200

    add common (common-o2)

---
+++ packages/common/package.py
@@ -0,0 +1,54 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install common
+#
+# You can edit this file again by typing:
+#
+#     spack edit common
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class Common(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://github.com/AliceO2Group/Common.git"
+    url      = "https://github.com/AliceO2Group/Common/archive/v1.4.9.tar.gz"
+    git = "https://github.com/AliceO2Group/Common.git"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('1.4.9', sha256='3ac14734488e7cf6a0ea7fda830f8e811a4483a662bb9bd5684c0aa6be182ea3')
+
+    # FIXME: Add dependencies if required.
+    # depends_on('foo')
+
+    depends_on('boost')
+    depends_on('cmake',type='build')
+    depends_on('ninja', type='build')
+
+    generator = 'Ninja'
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
