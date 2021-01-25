b00dff89a967a1453c8f4e6540f864d9f732bcb7
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2021-01-25 16:49:20 +0100

    GL on Mac changes

--- packages/openjdk/package.py
+++ packages/openjdk/package.py
@@ -6,7 +6,7 @@
 import os
 import platform
 import re
-
+import sys
 
 # If you need to add a new version, please be aware that:
 #  - versions in the following dict are automatically added to the package
@@ -16,6 +16,9 @@ import re
 #    format returned by platform.system() and 'arch' by platform.machine()
 
 _versions = {
+    '11.0.10_10': {
+        'Darwin-x86_64':('ee7c98c9d79689aca6e717965747b8bf4eec5413e89d5444cc2bd6dbd59e3811','https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.10%2B9/OpenJDK11U-jdk_x64_mac_hotspot_11.0.10_9.tar.gz')
+    },
     '11.0.8_10': {
         'Linux-x86_64': ('6e4cead158037cb7747ca47416474d4f408c9126be5b96f9befd532e0a762b47', 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz')},
     '11.0.0-2020-01-01': {
@@ -24,6 +27,9 @@ _versions = {
         'Linux-x86_64': ('99be79935354f5c0df1ad293620ea36d13f48ec3ea870c838f20c504c9668b57', 'https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz')},
     '11.0.1': {
         'Linux-x86_64': ('7a6bb980b9c91c478421f865087ad2d69086a0583aeeb9e69204785e8e97dcfd', 'https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz')},
+    '1.8.0_282_b08':{
+        'Darwin-x86_64':('1766d756f6e4a5d41b539f2ecf83e5a33e9336bd75f1602e8f4b4afbb8f47aaa','https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u282-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u282b08.tar.gz')
+    },
     '1.8.0_265-b01': {
         'Linux-x86_64': ('1285da6278f2d38a790a21148d7e683f20de0799c44b937043830ef6b57f58c4', 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz')},
     '1.8.0_191-b12': {
@@ -47,11 +53,12 @@ class Openjdk(Package):
         pkg = packages.get(key)
         if pkg:
             version(ver, sha256=pkg[0], url=pkg[1])
+            sver = ver.split('_',1)[0]
+            if re.match('11',sver):
+                provides('java@11',when='@{}'.format(ver))
+            if re.match('1.8',sver):
+                provides('java@8',when='@{}'.format(ver))
 
-    provides('java@11', when='@11.0:11.99')
-    provides('java@10', when='@10.0:10.99')
-    provides('java@9', when='@9.0:9.99')
-    provides('java@8', when='@1.8.0:1.8.999')
 
     conflicts('target=ppc64:', msg='openjdk is only available for x86_64 and aarch64')
     conflicts('target=ppc64le:', msg='openjdk is only available for x86_64 and aarch64')
@@ -70,6 +77,7 @@ class Openjdk(Package):
     def determine_version(cls, exe):
         output = Executable(exe)('-version', output=str, error=str)
 
+        print('output=',output)
         # Make sure this is actually OpenJDK, not Oracle JDK
         if 'openjdk' not in output:
             return None
@@ -97,6 +105,8 @@ class Openjdk(Package):
             version = str(self.version.up_to(2))
             prefix = java_home('--version', version, output=str).strip()
             prefix = Prefix(prefix)
+        elif sys.platform == 'darwin':
+            prefix = prefix + '/Contents/Home'
 
         return prefix
 
@@ -119,6 +129,7 @@ class Openjdk(Package):
         """Set JAVA_HOME."""
 
         env.set('JAVA_HOME', self.home)
+        env.set('JAVA_TOTO','TITI')
 
     def setup_dependent_build_environment(self, env, dependent_spec):
         """Set JAVA_HOME and CLASSPATH.
